87f54ba65acf9b10017055ddf8f603f664c7cb40
Zak Zebrowski <zak.zebrowski@gmail.com>
2016-04-17 19:51:29 -0400

    Create kapcodev2.py

---
+++ kapcodev2.py
@@ -0,0 +1,46 @@
+mport time
+import math
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+pre = [255, 0, 0]  # Red
+
+post = [0,255,0]
+
+sense.clear(pre)
+# Code translated from https://github.com/danjperron/mpu6050TestInC/tree/master
+Threshold = 0.1
+ShakeFlag = True;
+Counter = 1000;
+
+while (ShakeFlag):
+
+  data1 = sense.get_accelerometer_raw()
+
+  data2 = sense.get_accelerometer_raw()
+
+     #calculate new force
+  CurrentForce = math.sqrt( (data1['x'] - data2['x']) * ( data1['x'] - data2['x']) +
+                            (data1['y'] - data2['y']) * ( data1['y'] - data2['y']) +
+                            (data1['z'] - data2['z']) * ( data1['z'] - data2['z']))
+
+  print "Current: " , CurrentForce, "Counter: ", Counter
+  # If we are not shaking...
+  if CurrentForce < Threshold :
+    ShakeFlag = False
+  else :
+    Counter = Counter - 1;
+      if Counter < 1  :
+        ShakeFlag = False
+camera = picamera.PiCamera()
+camera.resolution=(1027,768)
+camera.iso=800
+sense.clear()
+camera.capture('image.jpg')
+sense.clear(post)
+time.sleep(2)
+sense.clear()
+print(sense.humidity)
+print(sense.temp)
+print(sense.pressure)
