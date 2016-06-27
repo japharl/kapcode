61aee3a33bd5f3aba5eb83cd24465020fa3c8ad6
Zak Zebrowski <zak.zebrowski@gmail.com>
2016-06-26 21:25:53 -0400

    Update message.py

--- message.py
+++ message.py
@@ -3,9 +3,8 @@
 from sense_hat import SenseHat
 import sys
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
-
 sense = SenseHat()
 
-sense.show_message(str(sys.argv))
+for arg in sys.argv[1:]:
+  sense.show_message(arg)
+  sense.show_message(" ")
