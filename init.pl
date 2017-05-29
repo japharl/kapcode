f4c413811ef64daa76052d928419ef4698c3f81e
Zak Zebrowski <zak.zebrowski@gmail.com>
2017-05-28 20:43:23 -0400

    Update init.pl

--- init.pl
+++ init.pl
@@ -1,18 +1,24 @@
+#!/usr/bin/perl
+
 use File::Path;
 use Proc::Background;
+use File::Slurper qw/write_text read_text/;
 
 mkpath '/home/pi/photos';
 mkpath '/media/usb';
 unlink '/home/pi/lock.txt';
 
 if (-e '/dev/sda1'){
-  print "USB Attached!";
-  my $pb0 = Proc::Background->new("mount -t vfat /dev/sda1 /media/usb");
-  $pb0->wait();
   my $pb1 = Proc::Background->new("cp /home/pi/photos/* /media/usb/");
   $pb1->wait();
   my $pb2 = Proc::Background->new("umount /media/usb");
   $pb2->wait();
   my $pb3 = Proc::Background->new("python /home/pi/message.py Files Coppied.");
   $pb3->wait();
+  system("halt -p now");
 }
+my $val = 0;
+if (!(-e "/home/pi/last.txt")){
+  write_text("/home/pi/last.txt",0);
+}
+
