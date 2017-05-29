457f00d12f20cb57060ef917943f122001b3ef02
Zak Zebrowski <zak.zebrowski@gmail.com>
2017-05-29 18:04:02 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 sudo perl /home/pi/kapcode/init.pl
+
+if [ -f /home/pi/kapcode/exit ]
+then
+  echo "Exit requested";
+  rm /home/pi/capcode/exit;
+  exit 0 ;
+fi
+
 sudo chown -R pi:pi /home/pi/kapcode/*
 sudo chmod -R 777 /home/pi/kapcode/*
 python /home/pi/kapcode/camera.py
