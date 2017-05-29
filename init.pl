9d9d13fd7ec1d09f42bb4724b57bdf2f09392bee
Zak Zebrowski <zak.zebrowski@gmail.com>
2017-05-29 18:05:51 -0400

    Update init.pl

--- init.pl
+++ init.pl
@@ -1,23 +1,31 @@
-#!/usr/bin/perl
-
 use File::Path;
 use Proc::Background;
 use File::Slurper qw/write_text read_text/;
 
 mkpath '/home/pi/kapcode/photos';
 mkpath '/media/usb';
+my $flag = 0;
 
 if (-e '/dev/sda1'){
+  my $pb0 = Proc::Background->new("mount -t vfat /dev/sda1 /media/usb");
+  $pb0->wait();
   my $pb1 = Proc::Background->new("cp /home/pi/kapcode/photos/* /media/usb/");
   $pb1->wait();
+  # If the file exit exists on the root of the usb device, the script will exit, not shutdown the pc, and will not start the camera script.
+  if (-e "/media/usb/exit"){
+    my $pbe = Proc::Background->new("touch /home/pi/kapcode/exit");
+    $pbe->wait();
+    $flag = 1;
+  }
   my $pb2 = Proc::Background->new("umount /media/usb");
   $pb2->wait();
   my $pb3 = Proc::Background->new("python /home/pi/kapcode/message.py Files Coppied.");
   $pb3->wait();
-  system("halt -p now");
+  if ($flag == 0){
+    system("halt -p");
+  }
 }
 my $val = 0;
 if (!(-e "/home/pi/kapcode/last.txt")){
   write_text("/home/pi/kapcode/last.txt",0);
 }
-
