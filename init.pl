ce6e9ddb4f741331360d679b07d3d12583806ef8
Zak Zebrowski <zak.zebrowski@gmail.com>
2017-05-29 17:34:15 -0400

    Update init.pl

--- init.pl
+++ init.pl
@@ -4,21 +4,20 @@ use File::Path;
 use Proc::Background;
 use File::Slurper qw/write_text read_text/;
 
-mkpath '/home/pi/photos';
+mkpath '/home/pi/kapcode/photos';
 mkpath '/media/usb';
-unlink '/home/pi/lock.txt';
 
 if (-e '/dev/sda1'){
-  my $pb1 = Proc::Background->new("cp /home/pi/photos/* /media/usb/");
+  my $pb1 = Proc::Background->new("cp /home/pi/kapcode/photos/* /media/usb/");
   $pb1->wait();
   my $pb2 = Proc::Background->new("umount /media/usb");
   $pb2->wait();
-  my $pb3 = Proc::Background->new("python /home/pi/message.py Files Coppied.");
+  my $pb3 = Proc::Background->new("python /home/pi/kapcode/message.py Files Coppied.");
   $pb3->wait();
   system("halt -p now");
 }
 my $val = 0;
-if (!(-e "/home/pi/last.txt")){
-  write_text("/home/pi/last.txt",0);
+if (!(-e "/home/pi/kapcode/last.txt")){
+  write_text("/home/pi/kapcode/last.txt",0);
 }
 
