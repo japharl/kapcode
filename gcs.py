9549b7474b558e81aba1d30fbe3f77412b21fb0e
Zak Zebrowski <zak.zebrowski@gmail.com>
2019-02-10 19:08:37 -0500

    Update gcs.py

--- gcs.py
+++ gcs.py
@@ -3,51 +3,77 @@
 from flask import Flask
 app = Flask(__name__)
 
-import dothat.lcd as lcd
+import time
 
-import dothat.backlight as backlight
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
 
-lines = {1:"Ready to connect",2:"",3:""}
-settings ={"contrast":49,"r":255,"g":255,"b":255,"hz":0}
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
-backlight.graph_set_led_duty(0,1)
+import subprocess
+
+# Note - a bunch of stuff here was directly imported from adafruit source code for rpi oled library.
+
+RST = None     # on the PiOLED this pin isnt used
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+draw = ImageDraw.Draw(image)
+
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Load default font.
+
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+#
+
+cmd = "hostname -I | cut -d\' \' -f1"
+IP = subprocess.check_output(cmd, shell = True )
+
+lines = {1:"IP:" + str(IP),2:"",3:"",4:""}
 
 def updateDisplay():
-        lcd.clear()
-        lcd.set_contrast(settings["contrast"])
-        backlight.graph_off()
-        backlight.graph_set_led_state(5-settings["hz"],1)
-        backlight.rgb(settings["r"],settings["g"],settings["b"])
-        lcd.set_cursor_position(0,0)
-        lcd.write(lines[1])
-        lcd.set_cursor_position(0,1)
-        lcd.write(lines[2])
-        lcd.set_cursor_position(0,2)
-        lcd.write(lines[3])
+        disp.clear()
+        disp.display()
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
+        draw.text((x, top), lines[1],  font=font, fill=255)
+        draw.text((x, top + 8), lines[2],  font=font, fill=255)
+        draw.text((x, top + 16), lines[3],  font=font, fill=255)
+        draw.text((x, top + 24), lines[4],  font=font, fill=255)
+        disp.image(image)
+        disp.display()
         return
 
 @app.route("/")
 def index():
-        return"set val"
+        return"/line/[1234]/string:val"
 @app.route('/line/<int:l>/<string:val>')
 def l1(l,val):
         lines[l]=val
         updateDisplay()
         return "ok"
-@app.route('/height/<int:hapi>')
-def heights(hapi):
-        settings["hz"]=hapi
-        updateDisplay()
-        return "ok"
-@app.route('/rgb/<int:r0>/<int:g0>/<int:b0>')
-def rgbd(r0,g0,b0):
-        settings["r"]=r0
-        settings["g"]=g0
-        settings["b"]=b0
-        updateDisplay()
-        return "ok"
-@app.route('/contrast/<int:c>')
-def contr(c):
-        settings["contrast"]=c
-        updateDisplay()
-        return "oK"
+
+lines[2]="Run this via flask"
+updateDisplay()
