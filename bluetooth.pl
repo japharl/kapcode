982f1c604037e764c18b02de16311d37cbdaa07c
Zak Zebrowski <zak.zebrowski@gmail.com>
2019-03-09 17:01:55 -0500

    Update bluetooth.pl

--- bluetooth.pl
+++ bluetooth.pl
@@ -11,5 +11,5 @@ open($OUT,">","/home/pi/.asoundrc");
 print $OUT "defaults.bluealsa.interface \"hci0\"\n";
 print $OUT "defaults.bluealsa.device \"$x\"\n";
 print $OUT "defaults.bluealsa.profile \"a2dp\"\n";
-print $OUT "defaults.bluealsa.delay \"10000\"\n";
+print $OUT "defaults.bluealsa.delay 10000\n";
 close $OUT;
